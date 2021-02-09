544fbc26bee08ed30aea56f4baf8683f546796d3
aakash <aakashbashyal@gmail.com>
2021-02-09 16:34:30 +0545

    email setting changed

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using BookStore.Application.Common.Interfaces;
 using BookStore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BookStore.Controllers
@@ -24,14 +28,40 @@ namespace BookStore.Controllers
         {
             int pageSize = 3;
 
-            var allBooks = await _bookService.GetAllBooks(currentPage,pageSize);
+            var allBooks = await _bookService.GetAllBooks(currentPage, pageSize);
 
             return View(allBooks);
 
         }
+        [Authorize, HttpGet("send-test-email")]
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
+            MailMessage mail = new MailMessage
+            {
+                Subject = "Hi",
+                Body = "This is the email body",
+                From = new MailAddress("devawsapp@gmail.com", "BookStoreTeam"),
+                IsBodyHtml = true
+            };
+
+            mail.To.Add("aakashbashyal");
+
+
+            NetworkCredential networkCredential = new NetworkCredential("devawsapp@gmail.com", "Admin@123");
+
+            SmtpClient smtpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = networkCredential
+            };
+
+            mail.BodyEncoding = Encoding.Default;
+
+            await smtpClient.SendMailAsync(mail);
             return View();
         }
 
